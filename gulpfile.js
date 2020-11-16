71fb887833395c35c8897eddbb1bb4616b82ebdb
Marina Hiuk <mlazor@mail.ru>
2020-11-16 11:42:52 +0100

    gul3 to gulp4 updated, add pug skill

--- gulpfile.js
+++ gulpfile.js
@@ -1,167 +1,86 @@
-"use strict";
+var gulp       = require('gulp'),
+	sass         = require('gulp-sass'),
+	browserSync  = require('browser-sync'),
+	concat       = require('gulp-concat'),
+	cssnano      = require('gulp-cssnano'),
+	rename       = require('gulp-rename'),
+	del          = require('del'),
+	imagemin     = require('gulp-imagemin'),
+	pngquant     = require('imagemin-pngquant'),
+	cache        = require('gulp-cache'),
+	autoprefixer = require('gulp-autoprefixer');
 
-var gulp = require("gulp"),
-  autoprefixer = require("gulp-autoprefixer"),
-  cssbeautify = require("gulp-cssbeautify"),
-  removeComments = require("gulp-strip-css-comments"),
-  rename = require("gulp-rename"),
-  sass = require("gulp-sass"),
-  cssnano = require("gulp-cssnano"),
-  rigger = require("gulp-rigger"),
-  uglify = require("gulp-uglify"),
-  watch = require("gulp-watch"),
-  plumber = require("gulp-plumber"),
-  run = require("run-sequence"),
-  rimraf = require("rimraf"),
-  webserver = require("browser-sync");
-
-/* Paths to source/build/watch files
-=========================*/
-
-var path = {
-  build: {
-    html: "build/",
-    js: "build/js/",
-    css: "build/css/",
-    img: "build/img/",
-    fonts: "build/fonts/",
-    json: "build/",
-  },
-  src: {
-    html: "src/*.{htm,html,php}",
-    js: "src/js/*.js",
-    css: "src/scss/style.scss",
-    img: "src/img/**/*.*",
-    fonts: "src/fonts/**/*.*",
-    json: "src/*.json",
-  },
-  watch: {
-    html: "src/**/*.{htm,html,php}",
-    js: "src/js/**/*.js",
-    css: "src/scss/**/*.scss",
-    img: "src/img/**/*.*",
-    fonts: "src/fonts/**/*.*",
-    json: "src/*.json",
-  },
-  clean: "./build",
-};
-
-/* Webserver config
-=========================*/
-
-var config = {
-  server: "build/",
-  notify: false,
-  open: true,
-  ui: false,
-};
-
-/* Tasks
-=========================*/
-
-gulp.task("webserver", function () {
-  webserver(config);
+gulp.task('sass', function() {
+	return gulp.src('src/scss/**/*style.scss')
+		.pipe(sass())
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+		.pipe(gulp.dest('src/css'))
+		.pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task("html:build", function () {
-  return gulp
-    .src(path.src.html)
-    .pipe(plumber())
-    .pipe(rigger())
-    .pipe(gulp.dest(path.build.html))
-    .pipe(webserver.reload({ stream: true }));
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'src'
+		},
+		notify: false
+	});
 });
 
-gulp.task("css:build", function () {
-  gulp
-    .src(path.src.css)
-    .pipe(plumber())
-    .pipe(sass().on("error", sass.logError))
-    .pipe(
-      autoprefixer({
-        browsers: ["last 8 versions"],
-        cascade: true,
-      })
-    )
-    .pipe(webserver.reload({ stream: true }))
-    .pipe(cssbeautify())
-    .pipe(gulp.dest(path.build.css))
-    .pipe(
-      cssnano({
-        zindex: false,
-        discardComments: {
-          removeAll: true,
-        },
-      })
-    )
-    .pipe(removeComments())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest(path.build.css))
-    .pipe(webserver.reload({ stream: true }));
+gulp.task('scripts', function() {
+	return gulp.src([
+		'src/js/main.js'
+		])
+		.pipe(concat('main.min.js'))
+		.pipe(gulp.dest('src/js'));
 });
 
-gulp.task("js:build", function () {
-  gulp
-    .src(path.src.js)
-    .pipe(plumber())
-    .pipe(rigger())
-    .pipe(gulp.dest(path.build.js))
-    .pipe(uglify())
-    .pipe(rename("main.min.js"))
-    .pipe(gulp.dest(path.build.js))
-    .pipe(webserver.reload({ stream: true }));
+gulp.task('code', function() {
+	return gulp.src('src/*.html')
+	.pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task("fonts:build", function () {
-  gulp.src(path.src.fonts).pipe(gulp.dest(path.build.fonts));
+gulp.task('clean', async function() {
+	return del.sync('build');
 });
 
-gulp.task("image:build", function () {
-  gulp.src(path.src.img).pipe(gulp.dest(path.build.img));
+gulp.task('img', function() {
+	return gulp.src('src/img/**/*')
+		.pipe(cache(imagemin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))/**/)
+		.pipe(gulp.dest('build/img'));
 });
 
-gulp.task("json:build", function () {
-  gulp.src(path.src.json).pipe(gulp.dest(path.build.json));
+gulp.task('prebuild', async function() {
+
+	gulp.src([
+		'src/css/style.css'
+		])
+	.pipe(gulp.dest('build/css'))
+
+	gulp.src('src/fonts/**/*')
+	.pipe(gulp.dest('build/fonts'))
+
+	gulp.src('src/js/**/*')
+	.pipe(gulp.dest('build/js'))
+
+	gulp.src('src/*.html')
+	.pipe(gulp.dest('build'));
+
 });
 
-gulp.task("clean", function (cb) {
-  rimraf(path.clean, cb);
-});
+gulp.task('clear', function (callback) {
+	return cache.clearAll();
+})
 
-gulp.task("build", function (cb) {
-  run(
-    "clean",
-    "html:build",
-    "css:build",
-    "js:build",
-    "fonts:build",
-    "image:build",
-    "json:build",
-    cb
-  );
+gulp.task('watch', function() {
+	gulp.watch('src/scss/**/*.scss', gulp.parallel('sass'));
+	gulp.watch('src/*.html', gulp.parallel('code'));
+	gulp.watch(['src/js/main.js'], gulp.parallel('scripts'));
 });
-
-gulp.task("watch", function () {
-  watch([path.watch.html], function (event, cb) {
-    gulp.start("html:build");
-  });
-  watch([path.watch.css], function (event, cb) {
-    gulp.start("css:build");
-  });
-  watch([path.watch.js], function (event, cb) {
-    gulp.start("js:build");
-  });
-  watch([path.watch.img], function (event, cb) {
-    gulp.start("image:build");
-  });
-  watch([path.watch.fonts], function (event, cb) {
-    gulp.start("fonts:build");
-  });
-  watch([path.watch.json], function (event, cb) {
-    gulp.start("json:build");
-  });
-});
-
-gulp.task("default", function (cb) {
-  run("clean", "build", "webserver", "watch", cb);
-});
+gulp.task('default', gulp.parallel('sass', 'scripts', 'browser-sync', 'watch'));
+gulp.task('build', gulp.parallel('prebuild', 'clean', 'img', 'sass', 'scripts'));
